f343033622da3f58ef4df418b3c63ed2b0a6283e
Onixfff <danilnikonov01@mail.ru>
2024-04-04 19:25:55 +0200

    Добавил отображения даже себя

--- Salary for workers/Form1.cs
+++ Salary for workers/Form1.cs
@@ -186,17 +186,13 @@ namespace Salary_for_workers
         private async Task<List<Worker>> GetWorkersAsync(int idPosition, string login, string password)
         {
             List<Worker> workers = new List<Worker>();
-            string query = "SELECT people.Id, people.Name, people.Surname, people.Patronymic, people.EmploymentDate FROM people JOIN passwords ON people.idPassword = passwords.id JOIN positions ON people.idPositions = positions.id WHERE passwords.login != @login AND passwords.password != @password AND positions.id = @idPositions;";
-            //sql для выбора даже себя
-            //string query = $"SELECT name, surname, Patronymic, EmploymentDate FROM authorization.people where idPositions = @idPositions;";
+            string query = "SELECT people.Id, people.Name, people.Surname, people.Patronymic, people.EmploymentDate FROM people JOIN passwords ON people.idPassword = passwords.id JOIN positions ON people.idPositions = positions.id WHERE positions.id = @idPositions;";
 
             try
             {
                 using (MySqlCommand command = new MySqlCommand(query, mCon))
                 {
                     command.Parameters.AddWithValue($"@idPositions", idPosition);
-                    command.Parameters.AddWithValue($"@password", password);
-                    command.Parameters.AddWithValue($"@login", login);
 
                     await mCon.OpenAsync();
 
